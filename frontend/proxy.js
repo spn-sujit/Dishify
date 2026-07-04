0f61f1f5b025658afcd3ea50628f43a99b8bf566
spn-sujit <nagasujitsoorneedi@gmail.com>
2026-07-04 17:05:42 +0530

    last2

--- frontend/proxy.js
+++ frontend/proxy.js
@@ -2,30 +2,40 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 import { aj } from './lib/arcjet';
 
-const isProtectedRoute = createRouteMatcher([
+const isProtectedRoute=createRouteMatcher([
   "/recipe(.*)",
   "/recipes(.*)",
   "/pantry(.*)",
   "/dashboard(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
-  const decision = await aj.protect(req);
-
-  if (decision.isDenied()) {
-    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+export default clerkMiddleware(async(auth,req)=>{
+  const decision=await aj.protect(req);
+   if (decision.isDenied()) {
+    if (decision.reason.isBot()) {
+      return NextResponse.json(
+        { error: "No bots allowed", reason: decision.reason },
+        { status: 403 },
+      );
+    } else {
+      return NextResponse.json(
+        { error: "Forbidden", reason: decision.reason },
+        { status: 403 },
+      );
+    }
   }
-
-  if (isProtectedRoute(req)) {
-    await auth().protect();
+  const {userId,redirectToSignIn}=await auth();
+  if(!userId && isProtectedRoute(req)){
+    return redirectToSignIn();
   }
-
   return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
